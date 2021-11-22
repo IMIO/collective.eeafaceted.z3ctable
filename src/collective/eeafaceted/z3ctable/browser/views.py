2dd8c4214aa0d95b052f91fe09dcf8fecd667119
fngaha <franck.o.ngaha@gmail.com>
2021-11-22 18:23:13 +0100

    Fix Py3 Errors

--- src/collective/eeafaceted/z3ctable/browser/views.py
+++ src/collective/eeafaceted/z3ctable/browser/views.py
@@ -11,7 +11,7 @@ from z3c.table.interfaces import INoneCell
 from z3c.table.table import SequenceTable
 from zope.component import getAdapters
 from zope.component import queryMultiAdapter
-from zope.interface import implements
+from zope.interface import implements, implementer
 
 import html
 import logging
@@ -112,9 +112,8 @@ class ExtendedCSSTable(SequenceTable):
         return renderedCell
 
 
+@implementer(IFacetedTable)
 class FacetedTableView(BrowserView, ExtendedCSSTable):
-
-    implements(IFacetedTable)
 
     # workaround so z3c.table does not manage batching
     startBatchingAt = 9999
